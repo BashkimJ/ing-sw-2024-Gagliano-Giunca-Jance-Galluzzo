7bed3043295ee8ded6e518346aa5255f7bbf0a28
samuelgiunca <samuel.giunca@mail.polimi.it>
2024-03-12 15:19:23 +0100

    Test2

--- CodexNaturalis/src/main/java/it/polimi/ingsw/App.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/App.java
@@ -7,6 +7,7 @@ package it.polimi.ingsw;
 public class App 
 {
     int i = 3;
+    private String toBeDeleted;
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
